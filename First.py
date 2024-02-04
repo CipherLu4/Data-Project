8591473bfead051caf4d3960cce40b6286f0d20e
CipherLu4 <jcarlos.lunatrj@gmail.com>
2024-02-04 04:29:43 -0600

    Adding first practice file

---
+++ First.py
@@ -0,0 +1,11 @@
+
+name = input("Ingresa tu nombre> ")
+last_name = input("Apellido> ")
+
+print("Hola " + name)
+
+#FORMAT
+
+#print("Hola, {} {}".format(name, last_name))
+print(f"Hola {name} {last_name}")
+
